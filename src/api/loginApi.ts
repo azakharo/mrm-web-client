46839e66725f10a978cafcc8892dccf3b2dd7d34
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2025-02-20 18:54:32 +0300

    added api methods for getting auth token

---
+++ src/api/loginApi.ts
@@ -0,0 +1,46 @@
+import {AxiosResponse} from 'axios';
+
+import {axi} from './loginApiAxiosSetup';
+
+export const getLoginProviders = async (): Promise<
+  Array<{
+    providerId: number;
+    appName: string;
+    appId: string;
+    authPageUrl: string;
+  }>
+> => {
+  const response = await axi.get<
+    Array<{
+      id: number;
+      name: string;
+      app_id: string;
+      auth_page_url: string;
+    }>
+  >('/api/oauth/providers');
+
+  return response.data.map(({id, name, app_id, auth_page_url}) => ({
+    providerId: id,
+    appName: name,
+    appId: app_id,
+    authPageUrl: auth_page_url,
+  }));
+};
+
+export const getAuthToken = async (
+  loginProviderId: number,
+  authCode: string,
+): Promise<string> => {
+  const response = await axi.post<
+    {
+      auth_code: string;
+    },
+    AxiosResponse<{
+      access_token: string;
+    }>
+  >(`/api/oauth/callback/${loginProviderId}`, {
+    auth_code: authCode,
+  });
+
+  return response.data.access_token;
+};
---
+++ src/api/loginApiAxiosSetup.ts
@@ -0,0 +1,24 @@
+import axios, {isAxiosError} from 'axios';
+
+export const axi = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+axi.interceptors.response.use(
+  response => response,
+  (error: unknown) => {
+    if (isAxiosError(error)) {
+      // Здесь не описываю detail в Typescript, просто проверяю наличие в runtime
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      const errorMessage1 = error.response?.data?.detail?.msg as string;
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      const errorMessage2 = error.response?.data?.detail as string;
+      const errorMessage = errorMessage1 || errorMessage2;
+      if (errorMessage) {
+        return Promise.reject(new Error(errorMessage));
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
