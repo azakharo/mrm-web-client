2f077d731d5704cbaf95a4ab28a8e30228d95dfa
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2025-02-21 15:55:08 +0300

    rem React.StrictMode because of 2 requests for auth token

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -8,7 +8,9 @@ const container = document.getElementById('root');
 const root = createRoot(container as Element);
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // Have to comment this, because 2 requests for the auth token happened.
+  // One from those request succeeded, another failed which sometime broke the app's flow.
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>,
 );
