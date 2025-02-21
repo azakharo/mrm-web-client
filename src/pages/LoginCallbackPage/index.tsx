88b53addffcb1075b63821358c8a7bd5570fbe5f
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2025-02-21 12:30:58 +0300

    impl getting auth token

--- src/pages/LoginCallbackPage/index.tsx
+++ src/pages/LoginCallbackPage/index.tsx
@@ -1,9 +1,45 @@
 import {FC} from 'react';
-import {useSearchParams} from 'react-router-dom';
+import {useNavigate, useParams, useSearchParams} from 'react-router-dom';
+import {Alert, Box, CircularProgress} from '@mui/material';
+import useRequest from 'ahooks/es/useRequest';
+
+import {getAuthToken} from '@/api';
+import {ROUTE__DASHBOARD} from '@/constants';
+import {setAuthToken} from '@/helpers/localStorage';
 
 export const LoginCallbackPage: FC = () => {
+  const navigate = useNavigate();
+  const {providerId: providerIdStr} = useParams();
+  const providerId = Number(providerIdStr);
   const [searchParams] = useSearchParams();
   const authCode = searchParams.get('code') ?? '';
 
-  return <div>{authCode}</div>;
+  const {loading, error} = useRequest(
+    () => getAuthToken(providerId, authCode),
+    {
+      onSuccess: authToken => {
+        setAuthToken(authToken);
+        navigate(ROUTE__DASHBOARD);
+      },
+    },
+  );
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      sx={{
+        height: '100dvh',
+        backgroundColor: '#F6F6F6',
+      }}
+      p={4}
+    >
+      <Box flex="0 1 360px">
+        {loading && <CircularProgress color="secondary" />}
+
+        {error && <Alert severity="error">{error.message}</Alert>}
+      </Box>
+    </Box>
+  );
 };
