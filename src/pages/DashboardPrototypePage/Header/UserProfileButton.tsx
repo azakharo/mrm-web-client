52dd89b9ec83a656c64a373c2a1dc21f8544ca77
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2025-02-26 16:18:40 +0300

    header: added user profile button

---
+++ src/pages/DashboardPrototypePage/Header/UserProfileButton.tsx
@@ -0,0 +1,69 @@
+import {FC} from 'react';
+import {Avatar, Box, ButtonBase, Stack, Typography} from '@mui/material';
+import {darken} from '@mui/material/styles';
+
+import userAvatarSrc from './userAvatar.png';
+
+import {COLOR__BACK, COLOR__WHITE} from '@/theme/colors';
+
+const backgroundColorOnHover = darken(COLOR__WHITE, 0.1);
+
+export const UserProfileButton: FC = () => {
+  // TODO get user info from useAuth()
+  const userName = 'Елена Викторовна';
+  const userPosition = 'Директор';
+
+  const handleClick = () => {
+    // TODO impl
+    alert('not implemented');
+  };
+
+  return (
+    <ButtonBase onClick={handleClick}>
+      <Box
+        p={1}
+        display="flex"
+        alignItems="center"
+        gap={2}
+        sx={{
+          backgroundColor: COLOR__WHITE,
+          border: `1px solid ${COLOR__BACK}`,
+          borderRadius: '12px',
+          '&:hover': {
+            backgroundColor: backgroundColorOnHover,
+          },
+          '&:active': {
+            backgroundColor: backgroundColorOnHover,
+          },
+        }}
+      >
+        <Avatar src={userAvatarSrc} alt={userName} sx={{marginLeft: 1}} />
+
+        <Stack alignItems="flex-start">
+          <Typography
+            noWrap
+            sx={{
+              fontWeight: 400,
+              fontSize: 14,
+              lineHeight: '18px',
+            }}
+          >
+            {userName}
+          </Typography>
+
+          <Typography
+            noWrap
+            sx={{
+              fontWeight: 400,
+              fontSize: 14,
+              lineHeight: '18px',
+              color: '#767485',
+            }}
+          >
+            {userPosition}
+          </Typography>
+        </Stack>
+      </Box>
+    </ButtonBase>
+  );
+};
--- src/pages/DashboardPrototypePage/Header/index.tsx
+++ src/pages/DashboardPrototypePage/Header/index.tsx
@@ -1,13 +1,22 @@
 import {FC} from 'react';
 import {Box, Typography} from '@mui/material';
 
+import {UserProfileButton} from './UserProfileButton';
+
 interface Props {
   title: string;
 }
 
 export const Header: FC<Props> = ({title}) => {
   return (
-    <Box display="flex" alignItems="center" px={4} pt={4} pb={3}>
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      px={4}
+      pt={4}
+      pb={3}
+    >
       <Typography
         sx={{
           fontWeight: 500,
@@ -17,6 +26,8 @@ export const Header: FC<Props> = ({title}) => {
       >
         {title}
       </Typography>
+
+      <UserProfileButton />
     </Box>
   );
 };
