d68689dabdb83107170f24a75b098d76d196a705
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2025-03-04 05:07:51 +0300

    finished Badge styling

--- src/pages/DashboardPrototypePage/Dashboard/TaskProgressStat/Value/index.tsx
+++ src/pages/DashboardPrototypePage/Dashboard/TaskProgressStat/Value/index.tsx
@@ -1,8 +1,10 @@
 import {FC} from 'react';
-import {Badge, Box, Stack, Typography} from '@mui/material';
+import {Badge, badgeClasses, Box, Stack, Typography} from '@mui/material';
 
 import {COLOR__LIGHT_GRAY, COLOR__LINE} from '@/theme/colors';
 import {Color} from '@/types/ui';
+
+const badgeSize = 10;
 
 interface Props {
   count: number;
@@ -11,39 +13,46 @@ interface Props {
 }
 
 export const Value: FC<Props> = ({count, badgeColor, label}) => {
-  console.log({badgeColor});
-
   return (
-    <Box
-      borderRadius="15px"
-      border={`0.5px solid ${COLOR__LINE}`}
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      px={2.5}
-      py={4}
-      minWidth={120}
+    <Badge
+      variant="dot"
+      sx={{
+        [`& .${badgeClasses.dot}`]: {
+          width: badgeSize,
+          height: badgeSize,
+          borderRadius: '50%',
+          backgroundColor: badgeColor,
+        },
+        '& .MuiBadge-badge': {
+          right: 16,
+          top: 16,
+        },
+      }}
     >
-      <Badge
-        badgeContent=""
-        color="success"
-        overlap="circular"
-        sx={{transform: 'translate(60px, -60px)'}}
-      ></Badge>
+      <Box
+        borderRadius="15px"
+        border={`0.5px solid ${COLOR__LINE}`}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        px={2.5}
+        py={4}
+        minWidth={120}
+      >
+        <Stack spacing={0.5} alignItems="center">
+          <Typography variant="h3bold">{count}</Typography>
 
-      <Stack spacing={0.5} alignItems="center">
-        <Typography variant="h3bold">{count}</Typography>
-
-        <Typography
-          variant="b2regular"
-          sx={{
-            color: COLOR__LIGHT_GRAY,
-          }}
-          noWrap
-        >
-          {label}
-        </Typography>
-      </Stack>
-    </Box>
+          <Typography
+            variant="b2regular"
+            sx={{
+              color: COLOR__LIGHT_GRAY,
+            }}
+            noWrap
+          >
+            {label}
+          </Typography>
+        </Stack>
+      </Box>
+    </Badge>
   );
 };
