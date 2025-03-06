f8bc16f0bd832303827049777cff0a174fa07579
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2025-03-06 12:32:39 +0300

    added tanstack query's dev tools

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter} from 'react-router-dom';
 import {AuthProvider} from '@features/auth';
 import {ThemeProvider} from '@mui/material/styles';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 
 import {isDevelopment, isProduction} from '@shared/constants';
 import GlobalStyles from './GlobalStyles';
@@ -52,6 +53,11 @@ const App = () => {
             <Routes />
           </AuthProvider>
         </BrowserRouter>
+
+        <ReactQueryDevtools
+          initialIsOpen={false}
+          buttonPosition="bottom-left"
+        />
       </QueryClientProvider>
     </ThemeProvider>
   );
